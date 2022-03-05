0263f65424efce9975393a6aa54b66a653cf1baa
nguyen.t.hoang-1@ou.edu <nguyen.t.hoang-1@ou.edu>
2022-03-05 11:11:39 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,98 @@
+$(function(){
+  $('#get-button').on('click',function(){     //when click button TWEETGET defined in html  
+    $.ajax({                               //using ajax to write code
+      url: '/arrayof3Items',               //access url data stored in serever
+      contentType: 'application/json',     //type of json
+      success: function(response){         //when suceess that function below is append these field and button in web
+        console.log(response);
+        var tbodyEl=$('tbody');             //using tbody to append
+        tbodyEl.html(''); 
+        response.arrayof3Items.forEach(function(arrayof3Items){          
+          tbodyEl.append('\
+          <tr>\
+          <td class="id" >' +arrayof3Items.id+'</td>\
+          <td><input type="text" class="created_at" value= "'+arrayof3Items.created_at+ '"></td>\
+          <td><input type="text" class="text" value= "'+arrayof3Items.text+ '"></td>\
+          <td><input type="text" class="screen_name" value="'+arrayof3Items.screen_name+ '"></td>\
+          <td>\
+          <button class ="update-button">UPDATE</button>\
+          <button class ="delete-button">DELETE</button>\
+          </td>\
+          </tr>\
+          ');
+        });   
+      }
+    });
+  });
+  
+
+    //CREATE/POST
+
+    $('#create-form').on('submit',function(event){  //when click on the button CREATE/POST to submit
+
+      event.preventDefault();                      //prevent the submit default
+      var createInput=$('#idcreate-input');        // create object createInput for id input field
+      var createInput1=$('#textcreate-input');     //create object createInput1 for text input field
+      $.ajax({
+        url:"/arrayof3Items",                      //access data of server
+        method: 'POST',                            //post data on server
+        contentType:'application/json',
+        data:JSON.stringify({id: createInput.val(),text: createInput1.val() }),  //stringify Json
+        success: function(response){                     //if success
+           console.log(response);
+           createInput.val('');
+           createInput1.val('');
+           $('#get-button').click();
+        }
+
+      })
+      })
+
+      //UPDATE /PUT
+
+    $('table').on('click','.update-button',function(){       //click on any button on table also to put
+      var rowEl=$(this).closest('tr');                     //using rowEl
+      var id=rowEl.find('.id').text();                     //use rowEl to find id
+      var screen_name=rowEl.find('.screen_name').val();    //use rowEl to find screen_name
+      var text=rowEl.find('.text').val();                  //use rowEl to find text
+      var created_at=rowEl.find('.created_at').val();      //use rowEl to find created_at
+
+      $.ajax({
+      url: '/arrayof3Items/'+'screen_name',                           //access id of data
+      method: 'PUT',
+      contentType: 'application/json',
+      data: JSON.stringify({screen_name: screen_name}),
+      success:function(response){
+      console.log(response);
+      $('#get-button').click();
+       }
+    })
+  })
+     //DELETE
+     $('table').on('click','.delete-button',function(){        //click on any button on table also to delete
+      var rowEl=$(this).closest('tr');
+      var id=rowEl.find('.id').text();
+      $.ajax({
+      url:'/arrayof3Items/'+id,
+      method:'DELETE',
+      contentType:'application/json',
+      success: function(response){
+      console.log(response);
+      $('#get-button').click();
+      }
+    })
+
+  })
+})
+     
+
+
+
+
+  
+      
+    
+    
+               
+    
+  
---
+++ server.js
@@ -0,0 +1,75 @@
+const express=require('express');                        //starting express
+const app=express();                                 //assign app to use these function of express module
+const fs=require('fs');                              //assign file system object to read the file favs.json
+var bodyParser= require('body-parser');              // using bodyParser to wrap data in order to put of get via script
+app.listen(3000,()=>console.log('listening at 3000'));   //function to reach server and get response via notice console.log
+app.use(express.static(__dirname));                    //read file index html
+app.use(bodyParser.json());                            //using bodyParser
+var arrayof3Items=[];                                  //create array to store these data from read file
+let k=0;
+fs.readFile('favs.json','utf8',function(err,data){       //function readfile
+    if(err) throw err;                                   //if read file error, get the notice
+    let readData=JSON.parse(data);                          //parse data and then store on variable readData
+    for(const eachItem of readData){                     //loop to get data
+      arrayof3Items[k]={                       
+        created_at:eachItem.created_at,                    //get created_at
+        text:eachItem.text,                              //get text
+        id:eachItem.user.id,                             //get userID
+        screen_name:eachItem.user.screen_name             //get screen_name
+    }
+    k=k+1;                                                //increament to store data via loop
+    console.log(arrayof3Items)                          // check data in console log
+    }   
+  
+    //Restful API get
+app.get('/arrayof3Items', function(req,res){    
+    res.send({arrayof3Items:arrayof3Items});
+  }) 
+
+    //Restful API post
+app.post('/arrayof3Items',function(req,res){
+   var assign3text=req.body.text;     //using body parser to get text data on scripts input and story via assign3text
+   var assign3User_ID=req.body.id;    //using body parse to get user_ID input on scripts file
+   
+  var text_empty="";                  
+   arrayof3Items.push({               //push these data on server
+      id:assign3User_ID,              //id
+      created_at:text_empty,          //created_at
+      text:assign3text,              //text
+      screen_name: text_empty         //screen_name
+   });
+   res.send('successfully created');
+})
+    //Restful API PUT OR UPDATE
+
+app.put('/arrayof3Items/:screen_name',function(req,res){   
+  var id=req.params.id;                         //wrap id 
+  var screen_name=req.body.screen_name;      //wrap screen_name that input and story newscreen_name
+  var found=false;                          
+  arrayof3Items.forEach(function(arrayof3Item,index){   //loop all items
+     if(!found&&arrayof3Item.screen_name==screen_name){                   
+        arrayof3Item.screen_name=screen_name;
+     }
+  })
+     res.send('successfully updated data');   //message display when successfully updated
+})
+  //Restful API DELETE 
+app.delete('/arrayof3Items/:id',function(req,res){
+  var id=req.params.id;                        //wrap id
+  var found=false;
+  arrayof3Items.forEach(function(arrayof3Item,index){ //loop
+     if(!found&&arrayof3Item.id==Number(id)){
+          arrayof3Items.splice(index,1);
+     }
+  })
+
+  res.send('successfully deleted product');
+})
+    
+
+
+
+
+   
+  
+});
